807584fb6c34ffb0243e8af01d86309be38e6886
Max Weller <igitt@max-weller.de>
2015-02-22 23:43:06 +0100

    increment version number to 6.14

--- ScreenGrab6.Alpha/My Project/AssemblyInfo.vb
+++ ScreenGrab6.Alpha/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.13.0.0")> 
-<Assembly: AssemblyFileVersion("6.13.0.0")> 
+<Assembly: AssemblyVersion("6.14.0.0")> 
+<Assembly: AssemblyFileVersion("6.14.0.0")> 
